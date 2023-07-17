e55db4f417f8ab40244adf14b94e44d915f4f4f0
Kyle Weston <kyle.weston@gmail.com>
2023-07-17 16:42:22 -0700

    - changed chunk size to 1000 - getting better results now - included title block in parsed out using arxivpdf

--- arxivpdf.py
+++ arxivpdf.py
@@ -4,10 +4,11 @@ from itertools import groupby
 import fitz
 import pandas as pd
 import re
+from langchain.docstore.document import Document
+from typing import List, Optional
 
 
 class ArxivPDF:
-
     def __init__(self, fname):
         self.fname = fname
 
@@ -21,19 +22,28 @@ class ArxivPDF:
         
     def get_text_dataframe(self) -> pd.DataFrame:
 
-
         with fitz.open(self.fname) as doc:
             # get the width and height of the first page
             dfs = []
             for i, page in enumerate(doc):
                 width, height = page.rect[2:]
+                centre = width // 2
                 pdata = page.get_text("blocks")
                 df = pd.DataFrame(pdata, columns=['x0', 'y0', 'x1', 'y1', 'text', 'block_no', 'block_type'])
                 # assume that text to the left of center are in the first column
                 # assume that text to the right of center are in the second column
-                # filter out blocks which cross columns
-                df_left =  df.loc[(df.x0 < width // 2) & (df.x1 < width // 2)]
-                df_right =  df.loc[(df.x0 > width // 2) & (df.x1 > width // 2)]
+                # try to extract the title and author list from the first page
+                # split left and right columns
+                # ignore blocks that span both columns
+                df_left =  df.loc[(df.x0 < centre) & (df.x1 < centre)]
+                df_right =  df.loc[(df.x0 > centre) & (df.x1 > centre)]
+
+                if i == 0:
+                    # Assume the title block is the first one that spans the centre column
+                    title_block = df.loc[(df.x0 < centre) & (df.x1 > centre) & (df.y0 < 0.2 * height)]
+                    # add title block to left column
+                    df_left = pd.concat([title_block, df_left])
+
                 df_combo = pd.concat([df_left, df_right])
                 # filter out images
                 df_combo = df_combo.loc[df_combo.block_type == 0]
@@ -46,29 +56,32 @@ class ArxivPDF:
 
         return pd.concat(dfs)
 
-    def get_text(self, split_sections=False) -> str:
-        """ Extract text from a an arxiv pdf in 2 column format"""
+    def split_text(self, split_sections=False) -> List[Document]:
+        """ Extract text from a an arxiv pdf in 2 column format
+        Args:
+            split_sections: if True, split the text into sections, otherwise split into pages
+        """
 
         df = self.get_text_dataframe()
         sections = [""]
-        section_names = []
+        section_names = ["None"]
         prev_page = -1
         for ind, row in df.iterrows():
             if split_sections:
                 if self._is_section_header(row):
                     sections.append("")
                     section_names.append(row.text.strip())
-
-            if row.page_no != prev_page:
-                sections[-1] += row.text
             else:
-                sections[-1] += row.text + "\n"
+                if row.page_no != prev_page:
+                    sections.append("")
+
+            sections[-1] += row.text + "\n"
             prev_page = row.page_no
         
         if split_sections:
-            return sections, section_names
+            return [Document(page_content=section, metadata={"section_name": section_name}) for section, section_name in zip(sections, section_names)]
         else:
-            return sections[0]
+            return [Document(page_content=section) for section in sections]
 
 
 
@@ -76,8 +89,9 @@ if __name__ == "__main__":
     fname = "2302.00923v4_clean.pdf"
     pdf = ArxivPDF(fname)
     print(f"Extracting text from {fname}")
-    text = pdf.get_text()
+    docs = pdf.split_text(True)
+
     outfname = fname.replace('pdf', 'txt')
     print(f"Writing to {outfname}")
     with open(outfname, 'w') as f:
-        f.write(text)
+        f.write("\n\n".join([page.page_content for page in docs]))
--- run.py
+++ run.py
@@ -1,69 +1,73 @@
-# %%
-with open('example.md', 'r') as f:
-    md_file = f.read()
+from arxivpdf import ArxivPDF
+import argparse
 
 
-# Let's create groups based on the section headers in our page
-from langchain.text_splitter import MarkdownHeaderTextSplitter
-headers_to_split_on = [
-    ("###", "Section"),
-]
-markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-md_header_splits = markdown_splitter.split_text(md_file)
+def main(fname, live_input=True):
 
-# convert section values to lowercase so that they match the queries
-for doc in md_header_splits:
-    if 'Section' in doc.metadata:
-        doc.metadata['Section'] = doc.metadata['Section'].lower()
-
-# Define our text splitter
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-chunk_size = 500
-chunk_overlap = 0
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-all_splits = text_splitter.split_documents(md_header_splits)
+    pdf = ArxivPDF(fname)
+    docs = pdf.split_text(split_sections=True)
+    # Define our text splitter
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(docs)
 
 
-# Build vectorstore and keep the metadata
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-vectorstore = Chroma.from_documents(documents=all_splits,
-                                    embedding=OpenAIEmbeddings())
+    # Build vectorstore and keep the metadata
+    from langchain.embeddings import OpenAIEmbeddings
+    from langchain.vectorstores import Chroma
+    vectorstore = Chroma.from_documents(documents=all_splits,
+                                        embedding=OpenAIEmbeddings())
 
-# Create retriever 
-from langchain.llms import OpenAI
-from langchain.retrievers.self_query.base import SelfQueryRetriever
-from langchain.chains.query_constructor.base import AttributeInfo
+    # Create retriever 
+    from langchain.llms import OpenAI
+    from langchain.retrievers.self_query.base import SelfQueryRetriever
+    from langchain.chains.query_constructor.base import AttributeInfo
 
-# Define our metadata
-metadata_field_info = [
-    AttributeInfo(
-        name="Section",
-        description="Part of the document that the text comes from",
-        type="string or list[string]",
-    ),
-]
-document_content_description = "Major sections of the document"
+    # Define our metadata
+    metadata_field_info = [
+        AttributeInfo(
+            name="Section",
+            description="Part of the document that the text comes from",
+            type="string or list[string]",
+        ),
+    ]
+    document_content_description = "Major sections of the document"
 
-# Define self query retriver
-llm = OpenAI(temperature=0)
-retriever = SelfQueryRetriever.from_llm(llm, vectorstore, document_content_description, metadata_field_info, verbose=True)
-out = retriever.get_relevant_documents("Summarize the motivation section of the document")
-print(out)
-pass
-
-
-# %%
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-llm = ChatOpenAI(temperature=0, verbose=True)
-qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever, verbose=True, return_source_documents=True)
-#out = qa_chain("Summarize the motivation section of this document")
-out = qa_chain("Summarize this document")
-print(out)
-pass
+    # Define self query retriver
+    llm = OpenAI(temperature=0)
+    retriever = SelfQueryRetriever.from_llm(llm, vectorstore, document_content_description, metadata_field_info, verbose=True)
+    # out = retriever.get_relevant_documents("Summarize the introduction section of the document")
+    # print(out)
 
 
+    from langchain.chains import RetrievalQA
+    from langchain.chat_models import ChatOpenAI
+    from langchain.memory import ConversationBufferMemory
+
+    llm = ChatOpenAI(temperature=0, verbose=True)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm, retriever=retriever,
+        verbose=True,
+        return_source_documents=False,
+        memory=ConversationBufferMemory()
+    )
+    #out = qa_chain("Summarize the motivation section of this document")
+    if live_input:
+        while True:
+            question = input("Enter a question (q to quit): ")
+            if question.strip() == "q":
+                break
+            answer = qa_chain(question)
+            print(f"query: {answer['query']}")
+            print(f"result: {answer['result']}")
+    else:
+        out = qa_chain("Give a concise summary of this document")
+        print(out)
 
 
-# %%
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Run doc QA on the given Arxiv PDF")
+    parser.add_argument("fname", type=str, help="Path to the PDF file")
+    args = parser.parse_args(["2302.00923v4_clean.pdf"])
+    main(args.fname)
